ec5c4f5b4e4314160cab473659994267879aa994
Tatiana Tuor <tatianatuor@gmail.com>
2021-11-04 15:50:09 +0100

    Added tests for chat related models

--- test/models/conversation_test.rb
+++ test/models/conversation_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ConversationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "conversation between 2 registered users" do
+    sender = User.new name: 'new', email: 'new@gmail.com', password: 'pw'
+    sender.save!
+
+    receiver = User.new name: 'old', email: 'old@gmail.com', password: 'pw'
+    receiver.save!
+
+    convo = Conversation.new sender: sender, recipient: receiver
+    convo.save!
+
+    assert_equal Conversation.all.first, convo
+  end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid message" do
+    sender = User.new name: 'new', email: 'new@gmail.com', password: 'pw'
+    sender.save!
+
+    receiver = User.new name: 'old', email: 'old@gmail.com', password: 'pw'
+    receiver.save!
+
+    convo = Conversation.new sender: sender, recipient: receiver
+    convo.save!
+
+    message = Message.new conversation: convo, user: sender, body: "Hello"
+    message.save!
+
+    assert_equal convo.messages.all.first, message
+    assert_equal sender.messages.all.first, message
+    refute_equal receiver.messages.all.first, message
+  end
+
+  test "message body can't be empty" do
+    sender = User.new name: 'new', email: 'new@gmail.com', password: 'pw'
+    sender.save!
+
+    receiver = User.new name: 'old', email: 'old@gmail.com', password: 'pw'
+    receiver.save!
+
+    convo = Conversation.new sender: sender, recipient: receiver
+    convo.save!
+
+    message = Message.new conversation: convo, user: sender, body: ""
+    refute message.valid?
+  end
 end
